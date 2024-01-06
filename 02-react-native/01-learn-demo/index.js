bc12d5de65ce220304ecfd0903ac26ceb88e9fe1
MineMine <gaoyuan_flying@163.com>
2024-01-06 20:04:35 +0800

    feat:增加权限文件等

--- 02-react-native/01-learn-demo/index.js
+++ 02-react-native/01-learn-demo/index.js
@@ -6,4 +6,11 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+if (Platform.OS === 'android') {
+  if (UIManager.setLayoutAnimationEnabledExperimental) {
+    console.log('enable ...');
+    UIManager.setLayoutAnimationEnabledExperimental(true);
+  }
+}
+
 AppRegistry.registerComponent(appName, () => App);
